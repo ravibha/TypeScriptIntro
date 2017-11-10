4603e1da600b820040fdb92eabcbe132c2c7b8bc
Ravi Bhargava <ravibha@microsoft.com>
2017-11-09 16:43:28 -0800

    Add HTMl file

--- src/demoJQueryIntegration.js
+++ src/demoJQueryIntegration.js
@@ -1,10 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var $ = require("jquery");
+
 $(document).ready(function () {
     var num1;
     var num2;
     var total;
+
     function isSumGreaterThanZero(number) {
         var result = parseFloat(number);
         var lessThanZero = $("#lessThanZero");
@@ -15,14 +14,15 @@ $(document).ready(function () {
             lessThanZero.text("false");
         }
     }
+
     $("#addButton").click(function () {
         total = 0;
         var additionResult = $("#additionResult");
         num1 = parseFloat($("#number1").val());
         num2 = parseFloat($("#number2").val());
+
         total = num1 + num2;
         additionResult.val(total);
         isSumGreaterThanZero(total);
     });
 });
-//# sourceMappingURL=demoJQueryIntegration.js.map
--- src/demoJQueryIntegration.ts
+++ src/demoJQueryIntegration.ts
@@ -1,10 +1,14 @@
-﻿import * as $ from "jquery";
+﻿// When strict mode and noimplicityany settings are set to true in tsconfig.json, errors will start showing up.
+// By assigning proper types to variables, these errors will go away, and we can get to strongly typed code.
+
+import * as $ from "jquery";
 
 $(document).ready(function () {
     var num1;
     var num2;
     var total;
 
+    
     function isSumGreaterThanZero(number) {
         var result = parseFloat(number);
         var lessThanZero = $("#lessThanZero");
